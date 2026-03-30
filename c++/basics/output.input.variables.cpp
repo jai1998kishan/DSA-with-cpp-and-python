bf540dd3c4f2b2cde70f3430fefc43ba8038f4c2
Jaikishan <jaikishangupta754@gmail.com>
2026-03-30 10:03:03 +0530

    starting dsa class with c++ and python in this is basics about the language

---
+++ c++/basics/output.input.variables.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    cout<<"Hello world"<<endl;
+
+    int a,b;
+    cout<<"Enter a and b"<<endl;
+
+    cin>>a>>b;
+    cout<<a+b;
+
+    return 0;
+}
