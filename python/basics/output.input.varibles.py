32ff0734b8d41691da799060e44bceeec129b89c
Jaikishan <jaikishangupta754@gmail.com>
2026-03-30 10:16:15 +0530

    how to take input as a integer in python

--- python/basics/output.input.varibles.py
+++ python/basics/output.input.varibles.py
@@ -7,8 +7,14 @@ print(a);
 print("a");
 
 # how to take input in python
-b = input("Enter value ");
-print("After the providing input: ",b)
+# b = input("Enter value "); # by defaut python see this as the string
+# c = input("Enter value of c: ");
+
+b = int(input("Enter value : ")) #this how we make it int
+c = int(input("Enter value : "))
+
+
+print("b + c :  ",b + c)
 
 
 
