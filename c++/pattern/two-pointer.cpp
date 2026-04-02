d5f573caa4e132bf909abdfc8f0b1134eea9a701
Jaikishan <jaikishangupta754@gmail.com>
2026-04-02 15:49:24 +0530

    two pointer notes

---
+++ c++/pattern/two-pointer.cpp
@@ -0,0 +1,16 @@
+/*
+
+Two Pointer
+
+- question should be array/linklist
+- sorted/sort
+- merge/remove duplicat/reagrange array
+- detect cycle
+- pair/triplets/quadruple
+- Do not use extra space
+
+*/
+
+
+
+
