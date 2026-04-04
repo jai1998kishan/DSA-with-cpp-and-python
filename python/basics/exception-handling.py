a87284b0d42e88cf9348a53e49dc92a923439187
Jaikishan <jaikishangupta754@gmail.com>
2026-04-04 12:57:17 +0530

    exception ahndling and oops

---
+++ python/basics/exception-handling.py
@@ -0,0 +1,12 @@
+# Exception Handling 
+
+try:
+    print("Enter any number")
+    x = int(input());
+    print("number you have entered: ",x)
+except:
+    print("Error");
+
+
+# why use try-except?
+To avoid progream crash
---
+++ python/basics/oop.py
@@ -0,0 +1,20 @@
+# Object-Oriented Programming (OOP)
+
+class Student:
+    def __init__(self, name):
+        self.name = name
+
+    def show(self):
+        print(self.name)
+
+s = Student("Moto")
+s.show()
+
+
+# what is  __init__ 
+# Constructor (run automatically).
+
+# what is self?
+# Refers to current object.
+
+
